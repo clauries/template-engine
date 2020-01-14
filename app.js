23c6debf91a54732b27ff2f2c0aa30142ffdefe7
clauries <55369403+clauries@users.noreply.github.com>
2020-01-14 12:01:15 -0800

    node questions complete and working

---
+++ app.js
@@ -0,0 +1,14 @@
+const Template = require("./lib/template");
+
+
+// Call to start program - starts by asking questions about manager
+const template = new Template();
+
+// Start making the template
+template.managerQs();
+
+// Testing if questions work;
+
+// template.engineerQs();
+// template.internQs();
+// template.addEmployee();
